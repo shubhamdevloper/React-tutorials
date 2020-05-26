8bf7f27077b9eca03d4aaaa07bcb2f1285a2ea52
Shubham <shubhamp@auberginesolutions.com>
2020-05-26 19:04:27 +0530

    add routing to burger builder project

--- src/App.js
+++ src/App.js
@@ -1,50 +1,23 @@
-import React, { Component, Suspense } from 'react';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import User from './containers/User';
-import Welcome from './containers/Welcome';
-
-const Posts = React.lazy(() => import('./containers/Posts'));
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
-  state = { showPosts: false };
-
-  modeHandler = () => {
-    this.setState(prevState => {
-      return { showPosts: !prevState.showPosts };
-    });
-  };
-
-  render() {
+  render () {
     return (
-      <React.Fragment>
-        <button onClick={this.modeHandler}>Toggle Mode</button>
-        {this.state.showPosts ? (
-          <Suspense fallback={<div>Loading...</div>}>
-            <Posts />
-          </Suspense>
-        ) : (
-          <User />
-        )}
-      </React.Fragment>
-      // <BrowserRouter>
-      //   <React.Fragment>
-      //     <nav>
-      //       <NavLink to="/user">User Page</NavLink> |&nbsp;
-      //       <NavLink to="/posts">Posts Page</NavLink>
-      //     </nav>
-      //     <Route path="/" component={Welcome} exact />
-      //     <Route path="/user" component={User} />
-      //     <Route
-      //       path="/posts"
-      //       render={() => (
-      //         <Suspense fallback={<div>Loading...</div>}>
-      //           <Posts />
-      //         </Suspense>
-      //       )}
-      //     />
-      //   </React.Fragment>
-      // </BrowserRouter>
+      <div>
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
+        </Layout>
+      </div>
     );
   }
 }
